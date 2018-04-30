4fa43dac230e4cde726b801ef0f0a6a8412248f0
Torben Jepsen <torbenjepsen@outlook.com>
2018-04-30 16:10:52 -0500

    put it starwars api

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,11 +1,25 @@
-var app = angular.module('myApp', []);
+var app = angular.module('StarWarsApp', []);
 console.log('client.js loaded');
 
-app.controller('MyController', function(){
-    console.log('MyController loaded');
+app.controller('StarWarsController', ['$http', function ($http) {
+    console.log('StarWarsController loaded');
 
     var self = this;
 
-    self.message = 'Hi There!'
+    self.message = 'Coding we are';
 
-});
+    $http({
+        method: 'GET',
+        url: 'https://swapi.co/api/people',
+        params: {
+            search: 'Skywalker',
+        }
+    })
+    .then(function(response) {
+        console.log('response from SWAPI people', response);
+    })
+    .catch(function(error) {
+        console.log('The force was not with us', error);
+    })
+
+}]);
